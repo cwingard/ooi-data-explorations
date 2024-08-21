50df0ba49f3a0495f13b11f9d2f31ebb7001c3f8
Christopher Wingard <cwingard@coas.oregonstate.edu>
2024-08-21 15:40:06 -0700

    Start creation of additional QARTOD test limits

---
+++ python/ooi_data_explorations/qartod/endurance/qartod_ce_optaa.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author Christopher Wingard
+@brief Load the OPTAA data from the uncabled, Coastal Endurance Surface
+    Moorings and processes the data to generate QARTOD Gross Range and
+    Climatology test limits
+"""
+TODO: "Still need to develop a methodology for these values."
+pass
